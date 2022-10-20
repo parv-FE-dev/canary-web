3569e99b5226457e54228216099254920c5553d5
parv-moxie <parv@plgworks.com>
2022-10-20 16:39:44 +0530

    Tweet card and search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TweetCardComponent } from './tweet-card/tweet-card.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { HomeNavComponent } from './home-nav/home-nav.component';
+import { TrendingSuggestionsComponent } from './trending-suggestions/trending-suggestions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TweetCardComponent,
+    SearchBarComponent,
+    HomeNavComponent,
+    TrendingSuggestionsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
---
+++ src/app/trending-suggestions/trending-suggestions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-suggestions',
+  templateUrl: './trending-suggestions.component.html',
+  styleUrls: ['./trending-suggestions.component.scss']
+})
+export class TrendingSuggestionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
