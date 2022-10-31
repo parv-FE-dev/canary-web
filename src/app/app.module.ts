514dcf082d39c5198ba276980b63708113e0816e
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2022-10-31 15:52:32 +0530

    Changed structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListsComponent } from './lists/lists.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
-import { TweetCardComponent } from './tweet-card/tweet-card.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-import { HomeNavComponent } from './home-nav/home-nav.component';
-import { TrendingSuggestionsComponent } from './trending-suggestions/trending-suggestions.component';
-import { EmptyListComponent } from './empty-list/empty-list.component';
-import { PersonliseFeedComponent } from './personlise-feed/personlise-feed.component';
-import { SuperPrivateListComponent } from './super-private-list/super-private-list.component';
-import { ThreadsComponent } from './threads/threads.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { TweetCardComponent } from './components/tweet-card/tweet-card.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HomeNavComponent } from './components/home-nav/home-nav.component';
+import { TrendingSuggestionsComponent } from './components/trending-suggestions/trending-suggestions.component';
+import { EmptyListComponent } from './components/empty-list/empty-list.component';
+import { PersonliseFeedComponent } from './components/personlise-feed/personlise-feed.component';
+import { SuperPrivateListComponent } from './components/super-private-list/super-private-list.component';
+import { ThreadsComponent } from './components/threads/threads.component';
 
 @NgModule({
   declarations: [
